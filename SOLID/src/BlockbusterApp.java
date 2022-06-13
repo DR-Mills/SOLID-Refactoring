dcd2ef8028e36f6bb0c817614d596a909cfe5da9
Dustin Mills <98778591+DR-Mills@users.noreply.github.com>
2022-06-13 11:08:11 -0400

    BlockbusterLab Clone

---
+++ SOLID/src/BlockbusterApp.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BlockbusterApp {
+
+	private static Scanner scnr = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		Validator validator = new Validator();
+		MovieDatabase database = new MovieDatabase();
+		ArrayList<Movie> moviesInStock = database.getMovieArrayList();
+
+		boolean continueBingeWatching = false;
+
+		System.out.println("Welcome to GC Blockbuster!");
+
+		do {
+			System.out.println("Our current movies include,");
+
+			for (int i = 0; i < moviesInStock.size(); i++) {    // prints movie menu
+				System.out.println(i + 1 + ". " + moviesInStock.get(i).getTitle());
+			}
+
+			int userMovieSelection = validator.integerWithinRange(
+					"\nPlease select the movie you want to watch. (enter the movie number) ", scnr, 1,
+					moviesInStock.size()) - 1;
+
+			Movie currentMovie = moviesInStock.get(userMovieSelection);
+
+			currentMovie.printInfo(); // prints selected movie information
+
+			boolean watchesMovie = validator.userContinueYorN("\nDo you want to watch the movie? y/n ", scnr);
+
+			if (watchesMovie) {
+				boolean userEngaged;
+
+				do {
+					currentMovie.play();
+					userEngaged = currentMovie.userEngaged();
+				} while (userEngaged);
+			}
+
+			continueBingeWatching = validator.userContinueYorN("Would you like to watch another movie?",
+					"Thanks, goodbye!", scnr);
+//			if (continueBlockbuster) {
+//				continueBingeWatching = true;
+//			} else {
+//				continueBingeWatching = false;
+//			}
+
+		} while (continueBingeWatching);
+	}
+}
---
+++ SOLID/src/VHS.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class VHS extends Movie {
+	
+	// instance variables
+	private int currentTime;
+
+	
+	// constructor
+	public VHS(String title, int runTime, ArrayList<String> scenes) {
+		super(title, runTime, scenes);
+	}
+
+	
+	// getters & setters
+	public int getCurrentTime() {
+		return currentTime;
+	}
+
+	public void setCurrentTime(int currentTime) {
+		this.currentTime = currentTime;
+	}
+
+
+
+	// methods
+	@Override
+	public void play() {
+		System.out.println();
+		System.out.println("Scene " + currentTime + ":\n" + getPrinterFriendlyScene(getScenes().get(currentTime)));
+		currentTime++;
+		if (currentTime > getScenes().size()-1) {
+			rewind();
+		}
+	}
+	
+	public void rewind() {  // be kind, please rewind
+		currentTime = 0;
+		System.out.println("\nMovie has ended.\n" + "Be kind, please rewind.\n" + "Movie will continue from the beginning unless you exit.\n\n");
+	}
+
+
+}
